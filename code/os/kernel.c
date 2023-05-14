00ab425e1d96117732bd03b8089d151de6a3a11d
timer <330070781@qq.com>
2023-05-14 22:20:06 +0800

    01:串口配置输出字符到consle上

--- code/os/kernel.c
+++ code/os/kernel.c
@@ -1,5 +1,11 @@
+extern void uart_init(void);
+extern void uart_puts(char *s);
+
+
 void start_kernel(void)
 {
+	uart_init();
+	uart_puts("hello RVOS !");
 	while (1) {}; // stop here!
 }
 
---
+++ code/os/types.h
@@ -0,0 +1,10 @@
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+// 定义无符号整型
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int  uint32_t;
+typedef unsigned long long uint64_t;
+
+#endif /* __TYPES_H__ */
