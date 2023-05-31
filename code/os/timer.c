805b1464b6663640be6042a1c57696017996a84c
timer <330070781@qq.com>
2023-05-31 13:27:07 +0800

    5.31:软件定时器

--- code/os/timer.c
+++ code/os/timer.c
@@ -14,6 +14,8 @@ extern void schedule(void);
 
 static uint32_t _tick = 0;
 
+#define MAX_TIMER 10
+static struct timer timer_list[MAX_TIMER];
 
 /* load timer interval(in ticks) for next timer interrupt.*/
 void timer_load(int interval)
@@ -27,6 +29,12 @@ void timer_load(int interval)
 
 void timer_init()
 {
+	/* 初始化timer */
+	for(int i=0; i<MAX_TIMER; i++)
+	{
+		timer_list[i].func = NULL;
+		timer_list[i].arg = NULL; 
+	}
 	/*
 	 * On reset, mtime is cleared to zero, but the mtimecmp registers 
 	 * are not reset. So we have to init the mtimecmp manually.
@@ -40,11 +48,77 @@ void timer_init()
 	//w_mstatus(r_mstatus() | MSTATUS_MIE);
 }
 
+struct timer *timer_create(void (*handler)(void *arg), void *arg, uint32_t timeout)
+{
+	if (NULL == handler || 0 == timeout) {
+		return NULL;
+	}
+	spin_lock();
+
+	struct timer *t = &(timer_list[0]);
+	for (int i = 0; i < MAX_TIMER; i++) {
+		if (NULL == t->func) {
+			break;
+		}
+		t++;
+	}
+	if (NULL != t->func) {
+		spin_unlock();
+		return NULL;
+	}
+
+	t->func = handler;
+	t->arg = arg;
+	t->timeout_tick = _tick + timeout;
+
+	spin_unlock();
+
+	return t;
+
+}
+
+void timer_delete(struct timer *timer)
+{
+	spin_lock();
+	struct timer *t = &(timer_list[0]);
+	for (int i = 0; i < MAX_TIMER; i++) {
+		if (t == timer) {
+			t->func = NULL;
+			t->arg = NULL;
+			break;
+		}
+		t++;
+	}
+
+	spin_unlock();
+}
+
+static inline void timer_check()
+{
+	struct timer *t = &(timer_list[0]);
+	for (int i = 0; i < MAX_TIMER; i++) {
+		if (NULL != t->func) {
+			if (_tick >= t->timeout_tick) {
+				t->func(t->arg);
+
+				/* once time, just delete it after timeout */
+				t->func = NULL;
+				t->arg = NULL;
+
+				break;
+			}
+		}
+		t++;
+	}
+}
+
 void timer_handler() 
 {
 	_tick++;
 	printf("tick: %d\n", _tick);
 
+	timer_check();
+	
 	timer_load(TIMER_INTERVAL);
 
 	schedule();
--- code/os/user.c
+++ code/os/user.c
@@ -1,25 +1,44 @@
 #include "os.h"
 
+#define DELAY 4000
+struct userdata {
+	int counter;
+	char *str;
+};
 
-#define DELAY 1000
-#define USE_LOCK
+/* Jack must be global */
+struct userdata person = {0, "Jack"};
+
+void timer_func(void *arg)
+{
+	if (NULL == arg)
+		return;
+
+	struct userdata *param = (struct userdata *)arg;
+
+	param->counter++;
+	printf("======> TIMEOUT: %s: %d\n", param->str, param->counter);
+}
+
 void user_task0(void)
 {
 	uart_puts("Task 0: Created!\n");
 
+	struct timer *t1 = timer_create(timer_func, &person, 3);
+	if (NULL == t1) {
+		printf("timer_create() failed!\n");
+	}
+	struct timer *t2 = timer_create(timer_func, &person, 5);
+	if (NULL == t2) {
+		printf("timer_create() failed!\n");
+	}
+	struct timer *t3 = timer_create(timer_func, &person, 7);
+	if (NULL == t3) {
+		printf("timer_create() failed!\n");
+	}
 	while (1) {
-#ifdef USE_LOCK
-		spin_lock();
-#endif
-		uart_puts("Task 0: Begin ... \n");
-		for (int i = 0; i < 5; i++) {
-			uart_puts("Task 0: Running... \n");
-			task_delay(DELAY);
-		}
-		uart_puts("Task 0: End ... \n");
-#ifdef USE_LOCK
-		spin_unlock();
-#endif
+		uart_puts("Task 0: Running... \n");
+		task_delay(DELAY);
 	}
 }
 
